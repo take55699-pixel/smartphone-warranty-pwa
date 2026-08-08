2b6662b5a9379950593a80ea551dc2031b09948a
Take Pixel <take55699@gmail.com>
2026-08-08 13:33:22 +0900

    Bump PWA cache after factual audit

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='smartphone-warranty-pwa-v2';
+const CACHE_NAME='smartphone-warranty-pwa-v3';
 const APP_SHELL=['./','./index.html','./data.js','./extra-data.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
