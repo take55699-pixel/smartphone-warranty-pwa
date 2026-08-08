eb90d35da31dbdc8518ca61a5a232ceca726d250
Take Pixel <take55699@gmail.com>
2026-08-08 13:21:14 +0900

    Cache Sakura insurance data for offline use

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='smartphone-warranty-pwa-v1';
-const APP_SHELL=['./','./index.html','./data.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE_NAME='smartphone-warranty-pwa-v2';
+const APP_SHELL=['./','./index.html','./data.js','./extra-data.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',event=>{const req=event.request;if(req.method!=='GET')return;const url=new URL(req.url);if(url.origin!==self.location.origin)return;event.respondWith(caches.match(req).then(cached=>cached||fetch(req).then(resp=>{const copy=resp.clone();caches.open(CACHE_NAME).then(cache=>cache.put(req,copy));return resp;}).catch(()=>caches.match('./index.html'))));});
