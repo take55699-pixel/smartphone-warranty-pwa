31713bba37e9ea61edf0252bb9b0b13d2d80108d
Take Pixel <take55699@gmail.com>
2026-08-11 20:19:40 +0900

    Add v8 Care launcher with valid PNG favicon

---
+++ applecare-pixelcare/sw-v8.js
@@ -0,0 +1,26 @@
+const CACHE_NAME='applecare-pixelcare-v8';
+const APP_SHELL=[
+  './app-v8.html',
+  './index.html',
+  './manifest-v8.webmanifest',
+  './icons/care-v8-192.png',
+  './icons/care-v7.svg'
+];
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  const url=new URL(event.request.url);
+  if(url.origin!==self.location.origin)return;
+  event.respondWith(fetch(event.request).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));
+    return response;
+  }).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./app-v8.html'))));
+});
